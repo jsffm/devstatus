d9c2fe64110ce679644ac3ba3bb5cb1c15cb0872
kamel5 <kamel5 (at) gmx (dot) net>
2018-07-20 15:16:33 +0200

    Initial release

---
+++ i18n.c
@@ -0,0 +1,137 @@
+/*
+ * A plugin for the Video Disk Recorder
+ *
+ * See the README file for copyright information and how to reach the author.
+ *
+ */
+
+#include "i18n.h"
+
+const tI18nPhrase Phrases[] = {
+  { "display the current recordings on each device",
+    "Anzeige der laufenden Aufnahmen je Gerät",// Deutsch
+    "",// TODO Slovenski
+    "",
+    "",// TODO Nederlands
+    "",// TODO Portugues
+    "",
+    "",// TODO Norsk
+    "",
+    "",// TODO Polski
+    "",// TODO Español
+    "",// TODO ÅëëçíéêÜ (Greek)
+    "",// TODO Svenska
+    "",// TODO Romaneste
+    "",// TODO Magyar
+    "",// TODO Català
+    "",// TODO ÀãááÚØÙ (Russian)
+    "",// TODO Hrvatski (Croatian)
+#if VDRVERSNUM && VDRVERSNUM >= 10313
+    "", // Eesti
+#if VDRVERSNUM >= 10316
+    "", // Dansk
+#endif
+#endif
+  },
+  { "This plugin has no info text!",
+    "Dieses Plugin hat keinen Infotext!",
+    "",// TODO
+    "Questo plugin non ha ulteriori informazioni",// TODO
+    "",// TODO
+    "",// TODO
+    "Le plugin n'a pas de text d'info",
+    "",// TODO
+    "Laajennoksella ei ole infotekstiä!",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+#if VDRVERSNUM && VDRVERSNUM >= 10313
+    "", // Eesti
+#if VDRVERSNUM >= 10316
+    "", // Dansk
+#endif
+#endif
+  },
+  { "Recording status",
+    "Aufnahmestatus",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",
+    "",// TODO
+    "",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+#if VDRVERSNUM && VDRVERSNUM >= 10313
+    "", // Eesti
+#if VDRVERSNUM >= 10316
+    "", // Dansk
+#endif
+#endif
+  },
+  { "Device",
+    "Gerät",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",
+    "",// TODO
+    "",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+#if VDRVERSNUM && VDRVERSNUM >= 10313
+    "", // Eesti
+#if VDRVERSNUM >= 10316
+    "", // Dansk
+#endif
+#endif
+  },
+  { "no current recordings",
+    "keine laufenden Aufnahmen",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",
+    "",// TODO
+    "",
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+    "",// TODO
+#if VDRVERSNUM && VDRVERSNUM >= 10313
+    "", // Eesti
+#if VDRVERSNUM >= 10316
+    "", // Dansk
+#endif
+#endif
+  },
+  { NULL }
+  };
